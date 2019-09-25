0c2c4fa6a75a327e02406abf579cdb3edd280141
cxDosx <591701074@qq.com>
2019-09-25 16:04:17 +0800

    509斐波那契数

---
+++ src/problemSet/fib.java
@@ -0,0 +1,36 @@
+package problemSet;
+
+/**
+ * 题库
+ * 509. 斐波那契数
+ * Easy
+ *
+ * https://leetcode-cn.com/problems/fibonacci-number/
+ *
+ * https://leetcode-cn.com/submissions/detail/31175927/
+ */
+public class fib {
+
+    public static void main(String[] args) {
+        new fib(28);
+    }
+
+    public fib(int N) {
+        System.out.println(fib(N));
+    }
+
+    public int fib(int N){
+
+        return f(N);
+    }
+
+    public int f(int N){
+        if (N == 0) {
+            return N;
+        }
+        if (N == 1 || N == 2) {
+            return 1;
+        }
+        return f(N-1) + f (N-2);
+    }
+}
