53618dbd29d98092afc2efd09f24c6bcff94cb45
cxdosx <591701074@qq.com>
2019-10-14 11:03:47 +0800

    update 5222. Split a string in balanced strings

---
+++ src/weeklyContest/SplitAStringInBalancedStrings.java
@@ -0,0 +1,40 @@
+package weeklyContest;
+
+/**
+ * 周赛158
+ * 5222. 分割平衡字符串
+ * Easy
+ *
+ * https://leetcode-cn.com/contest/weekly-contest-158/problems/split-a-string-in-balanced-strings/
+ *
+ * https://leetcode-cn.com/submissions/detail/32984324/
+ */
+
+public class SplitAStringInBalancedStrings {
+
+    public static void main(String[] args) {
+        new SplitAStringInBalancedStrings("RLRRLLRLRL");
+    }
+
+    public SplitAStringInBalancedStrings(String s) {
+        System.out.println(balancedStringSplit(s));
+    }
+
+    public int balancedStringSplit(String s) {
+        int result = 0;
+        int L = 0;
+        int R = 0;
+        for (char c : s.toCharArray()){
+            if (c == 'L'){
+                L++;
+            }else{
+                R++;
+            }
+            if (L != 0 && L == R){
+                result ++;
+                L = R = 0;
+            }
+        }
+        return result;
+    }
+}
